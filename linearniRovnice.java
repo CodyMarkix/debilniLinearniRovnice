5ac98c92baadce4a2c1bdaf1e27e845b596c7b76
CodyMarkix <marekplasek08@protonmail.com>
2024-09-24 19:54:30 +0200

    changed filename

---
+++ linearniRovnice.java
@@ -0,0 +1,14 @@
+class linearniRovnice {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 5;
+        double x;
+
+        if (a > 0) {
+            x = -b / a;
+            System.out.println(String.format("X is this: %s", x));
+        } else {
+            System.out.println("youre a dum dum");
+        }
+    }
+}
